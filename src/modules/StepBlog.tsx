81a695280460431a6c8e7c2d7545f03372d6ed9a
leninner <mazabandalenin180@gmail.com>
2023-03-22 21:26:06 -0500

    add a blog card to show the blogs

--- src/modules/StepBlog.tsx
+++ src/modules/StepBlog.tsx
@@ -1,10 +1,18 @@
+import { Card } from '../components/Card'
+
 export const StepBlog = () => (
   <section
-    className="flex justify-center flex-col items-center h-[680px] bg-[#1a1b1c] text-white"
+    className="flex flex-col justify-evenly items-center md:h-[680px] bg-[#1a1b1c] text-red-400 md:px-[8.33vw] w-full py-10 gap-5"
     id="blog"
   >
-    <h1 className="text-6xl font-bold">Lenin Mazabanda</h1>
-    <h3 className="text-4xl">Software Developer</h3>
-    <p className="text-2xl font-bold">Be Kind to Yourself</p>
+    <h1>
+      Mira a tu <strong>alrededor</strong>
+    </h1>
+
+    <div className="flex flex-col md:flex-row justify-between items-center w-full md:gap-0 gap-5">
+      <Card />
+      <Card />
+      <Card />
+    </div>
   </section>
 )
--- src/modules/StepIntroduction.tsx
+++ src/modules/StepIntroduction.tsx
@@ -1,10 +1,10 @@
 import { GoToNextSection } from '../components/GoToNextSection'
 
 export const StepIntroduction = () => (
-  <section className="flex justify-center flex-col items-center h-screen bg-[#f9ef2e] overflow-hidden">
-    <h1 className="text-6xl font-bold">Lenin Mazabanda</h1>
-    <h3 className="text-4xl">Software Developer</h3>
-    <p className="text-2xl font-bold">Be Kind to Yourself</p>
+  <section className="flex justify-center flex-col items-center h-screen bg-[#f9ef2e] text-center">
+    <h1 className="text-4xl md:text-6xl font-bold">Lenin Mazabanda</h1>
+    <h3 className="text-2xl md:text-4xl">Software Developer</h3>
+    <p className="md:text-2xl font-bold">Be Kind to Yourself</p>
 
     <GoToNextSection extraClasses="mt-10" />
   </section>
