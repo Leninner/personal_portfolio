7d75d56b89d3fb15dfeeb7a3929408587041e356
leninner <mazabandalenin180@gmail.com>
2023-03-22 21:26:17 -0500

    add a blog card to show the blogs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,5 @@
-import { useState } from 'react'
-
-interface IPost {
-  id: number
-}
-
-export const Card = () => {
-  const [posts, setPosts] = useState<IPost[]>([])
-
-  return (
-    <div className="h-60 md:h-60 w-5/6 md:w-[30%] bg-red-50 outline-dashed">
-      <h1>Leninsin el amigos</h1>
-    </div>
-  )
-}
+export const Card = () => (
+  <div className="h-60 md:h-60 w-5/6 md:w-[30%] bg-red-50 outline-dashed">
+    <h1>Leninsin el amigos</h1>
+  </div>
+)
