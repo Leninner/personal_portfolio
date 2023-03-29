c9423afdc69f37b140a1cddef7ecea64e3cec2ec
leninner <lenin@tinkin.one>
2023-03-28 22:00:01 -0500

    Add summary section

---
+++ src/modules/StepSummary.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../components/Button'
+
+export const StepSummary = () => (
+  <div className="h-screen flex flex-col md:flex-row justify-between items-center md:h-[680px] md:px-[8.33vw] w-full py-10 gap-5">
+    <div className="flex-1">
+      <h1 className="text-4xl font-bold mb-5">
+        La juventud eterna para el código de tu negocio
+      </h1>
+
+      <p className="text-xl text-gray-600">
+        GeneXus es la Plataforma Empresarial Low-Code para desarrollo de
+        software, impulsada por Inteligencia Artificial, que simplifica y
+        automatiza las tareas de creación, evolución y mantenimiento de
+        aplicaciones empresariales y sistemas IT.
+      </p>
+
+      <Button
+        text="Descubre GeneXus"
+        type="anchor"
+        href="https://www.genexus.com/"
+        extraClasses="shadow-sm shadow-[#f9ef2e77] bg-[#f9ef2e] text-[#333] py-3 px-10 mt-5 text-lg"
+      />
+    </div>
+
+    <div className="flex-1 grid place-content-end">
+      <img src="" alt="" className="w-96 h-96 rounded-full bg-red-400" />
+    </div>
+  </div>
+)
