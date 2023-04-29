8b2801a976a84cc381c5a93a19e890ed20bdb5b5
leninner <mazabandalenin180@gmail.com>
2023-04-29 16:43:39 -0500

    add TS types

--- src/pages/api/blogs.ts
+++ src/pages/api/blogs.ts
@@ -1,7 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { HTTP_STATUS } from '../../constants/appEnums'
 import { getBlogs } from '../../services/getBlogs'
 
-const handler = async (_: unknown, res: any) => {
+const handler = async (_: NextApiRequest, res: NextApiResponse) => {
   const articles = await getBlogs()
 
   res.status(HTTP_STATUS.SUCCESS).json(articles)
--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,7 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { HTTP_STATUS } from '../../constants/appEnums'
 import { getProjects } from '../../services/getProjects'
 
-const handler = async (_: unknown, res: any) => {
+const handler = async (_: NextApiRequest, res: NextApiResponse) => {
   const articles = await getProjects()
 
   res.status(HTTP_STATUS.SUCCESS).json(articles)
